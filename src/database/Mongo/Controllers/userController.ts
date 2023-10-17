02d4e1b9e783af8e10a32fd536249b1af565e93f
zacharie <zacharie@tshoko.fr>
2023-10-17 13:28:34 +0200

    finish user controlleur

--- src/database/Mongo/Controllers/userController.ts
+++ src/database/Mongo/Controllers/userController.ts
@@ -6,12 +6,11 @@ const bcrypt = require('bcrypt');
 
 async function createUser(req: Request, res: Response) {
   try {
+    const { username, password } = req.body;
 
-    if (!req.body || !req.body.username || !req.body.password) {
+    if (!req.body || !username || !password) {
       return res.status(400).json({ message: "Invalid request body" });
     }
-
-    const { username, password } = req.body;
 
     let hash = await bcrypt.hash(password, 5);
     const newUser = new User({username: username, password: hash, profilePic: pictures.pickRandom()});
@@ -20,10 +19,74 @@ async function createUser(req: Request, res: Response) {
     res.status(200).send(newUser);
   }
   catch(error) {
-    console.log(error);
+    res.status(500).send("Internal Server Error");
   }
 }
 
+async function getUserByName(req: Request, res: Response) {
+  try {
+    const { username } = req.params;
+
+    if (!req.body || !username) {
+      return res.status(400).json({ message: "Invalid request body" });
+    }
+
+    const user = await User.findOne({username: username}).catch(() => res.status(500).send("User don't exist"));
+
+    res.status(200).send(user);
+  }
+  catch(error) {
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+async function getUserById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+
+    console.log(id);
+
+    if (!req.body || !id) {
+      return res.status(400).json({ message: "Invalid request body" });
+    }
+
+    const user = await User.findOne({_id: id}).catch(() => res.status(500).send("User don't exist"));
+
+    res.status(200).send(user);
+  }
+  catch(error) {
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+async function login(req: Request, res: Response) {
+  try {
+    const { username, password } = req.body;
+
+    if (!req.body || !username || !password) {
+      return res.status(400).json({ message: "Invalid request body" });
+    }
+
+    const user = await User.findOne({username: username}).catch(() => res.status(500).send("Internal error"));
+    if(!user) {
+      res.status(400).send("User not found")
+    }
+
+    let pwdCorrect = await bcrypt.compare(password, user.password)
+    if(!pwdCorrect) {
+      return res.status(400).send("Incorrect password");
+    }
+
+    res.status(200).send(user);
+  }
+  catch(error) {
+    res.status(500).send("Error : " + error)
+  }
+};
+
 module.exports = {
-  createUser
+  createUser,
+  getUserByName,
+  getUserById,
+  login
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,5 +3,8 @@ const router = express.Router()
 const userController = require('../database/Mongo/Controllers/userController');
 
 router.post('/create', userController.createUser);
+router.post('/login', userController.login);
+router.get('/name/:username', userController.getUserByName);
+router.get('/:id', userController.getUserById);
 
 module.exports = router;
